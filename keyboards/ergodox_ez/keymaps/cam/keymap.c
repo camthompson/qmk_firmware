2d0d703e960cafefb6d498575a5e90ffc351fce3
Cam Thompson <cam@camthompson.com>
2016-07-25 11:29:33 -0400

    Clean up layout comments

--- keyboards/ergodox_ez/keymaps/cam/keymap.c
+++ keyboards/ergodox_ez/keymaps/cam/keymap.c
@@ -20,7 +20,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------| Hyper|           | ~L1  |------+------+------+------+------+--------|
  * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |   /  | RShift |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   | Mute |  `~  |  \   | Left | Right|                                       | Left | Down |  Up  | Right|Ply/Pse |
+ *   | Mute |  `~  |  \   | Left | Right|                                       | Left | Down |  Up  | Right|Ply/Pse|
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
  *                                        | LAlt | LCtl |       | RCtl |RAlt  |
@@ -34,24 +34,23 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // Otherwise, it needs KC_*
 [BASE] = KEYMAP(  // layer 0 : default
         // left hand
-        KC_EQL,         KC_1,         KC_2,   KC_3,   KC_4,   KC_5,   KC_ESC,
-        KC_TAB,        KC_Q,         KC_W,   KC_E,   KC_R,   KC_T,   KC_BSLS,
-        CTL_T(KC_ESC),        KC_A,         KC_S,   KC_D,   KC_F,   KC_G,
-        KC_LSPO,        KC_Z,  KC_X,   KC_C,   KC_V,   KC_B,   ALL_T(KC_NO),
-        KC_MUTE,KC_GRV,      KC_BSLS,  KC_LEFT,KC_RGHT,
-                                              KC_LALT,  KC_LCTL,
-                                                              KC_HOME,
-                                               KC_ENT,GUI_T(KC_NO),KC_END,
+        KC_EQL        , KC_1   , KC_2    , KC_3    , KC_4    , KC_5 , KC_ESC       ,
+        KC_TAB        , KC_Q   , KC_W    , KC_E    , KC_R    , KC_T , KC_BSLS      ,
+        CTL_T(KC_ESC) , KC_A   , KC_S    , KC_D    , KC_F    , KC_G ,
+        KC_LSPO       , KC_Z   , KC_X    , KC_C    , KC_V    , KC_B , ALL_T(KC_NO) ,
+        KC_MUTE       , KC_GRV , KC_BSLS , KC_LEFT , KC_RGHT ,
+                                                        KC_LALT     , KC_LCTL      ,
+                                                                      KC_HOME      ,
+                                              KC_ENT , GUI_T(KC_NO) , KC_END       ,
         // right hand
-             KC_ESC,     KC_6,   KC_7,   KC_8,   KC_9,
-             KC_0,             KC_MINS,
-             KC_LBRC,       KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,             KC_RBRC,
-                          KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN, KC_QUOT,
-             MO(MDIA),KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,   KC_RSPC,
-                                  KC_LEFT,  KC_DOWN,KC_UP,KC_RIGHT,          KC_MPLY,
-             KC_RCTL,        KC_RALT,
-             KC_PGUP,
-             KC_PGDN,KC_BSPC, KC_SPC
+        KC_ESC        , KC_6    , KC_7     , KC_8    , KC_9     , KC_0    , KC_MINS ,
+        KC_LBRC       , KC_Y    , KC_U     , KC_I    , KC_O     , KC_P    , KC_RBRC ,
+                        KC_H    , KC_J     , KC_K    , KC_L     , KC_SCLN , KC_QUOT ,
+        MO(MDIA)      , KC_N    , KC_M     , KC_COMM , KC_DOT   , KC_SLSH , KC_RSPC ,
+                        KC_LEFT ,  KC_DOWN , KC_UP   , KC_RIGHT , KC_MPLY ,
+        KC_RCTL , KC_RALT,
+        KC_PGUP ,
+        KC_PGDN , KC_BSPC , KC_SPC
     ),
 /* Keymap 1: Media Layer
  *
@@ -77,23 +76,23 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // MDIA
 [MDIA] = KEYMAP(
        // left hand
-       KC_TRNS,KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
-       KC_TRNS,KC_TRNS,KC_TRNS,  KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,
-       KC_TRNS,KC_TRNS,KC_TRNS, KC_TRNS,KC_TRNS,KC_TRNS,
-       KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,
-       KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,
-                                       KC_TRNS,KC_TRNS,
-                                               KC_TRNS,
-                               KC_TRNS,KC_TRNS,KC_TRNS,
+       KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS ,
+       KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS ,
+       KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS ,
+       KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS ,
+       KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS ,
+                                                         KC_TRNS , KC_TRNS ,
+                                                                   KC_TRNS ,
+                                               KC_TRNS , KC_TRNS , KC_TRNS ,
        // right hand
-       KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS,   KC_TRNS,   KC_TRNS,  KC_TRNS,
-       KC_TRNS, KC_VOLD, KC_VOLU,KC_TRNS,    KC_TRNS,    KC_TRNS, KC_TRNS,
-                KC_TRNS, KC_TRNS,   KC_TRNS,    KC_TRNS,    KC_TRNS, KC_TRNS,
-       KC_TRNS, KC_MPRV, KC_MNXT,   KC_TRNS,    KC_TRNS,    KC_TRNS, KC_TRNS,
-                         KC_TRNS,KC_TRNS,  KC_TRNS,    KC_TRNS,  KC_TRNS,
-       KC_TRNS, KC_TRNS,
-       KC_TRNS,
-       KC_TRNS, KC_TRNS, KC_TRNS
+       KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS ,
+       KC_TRNS , KC_VOLD , KC_VOLU , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS ,
+                 KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS ,
+       KC_TRNS , KC_MPRV , KC_MNXT , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS ,
+                           KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS ,
+       KC_TRNS , KC_TRNS ,
+       KC_TRNS ,
+       KC_TRNS , KC_TRNS , KC_TRNS
 ),
 };
 
