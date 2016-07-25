6e9777f7a989a66853ca2ef4bb3a195a0abfa426
Cam Thompson <cam@camthompson.com>
2016-07-25 17:04:19 -0400

    Disable sleep LED in correct location

--- keyboards/ergodox_ez/keymaps/cam/config.h
+++ keyboards/ergodox_ez/keymaps/cam/config.h
@@ -3,10 +3,6 @@
 
 #include "../../config.h"
 
-// Disable LED blinking during sleep
-#undef SLEEP_LED_ENABLE
-#define SLEEP_LED_ENABLE no
-
 // Tap left shift for (, right shift for {
 #define LSPO_KEY KC_9
 #define RSPC_KEY KC_LBRC
