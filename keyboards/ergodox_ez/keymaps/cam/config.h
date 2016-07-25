a227b12998d5801ba44b6e594c2f92d3dc4e79f2
Cam Thompson <cam@camthompson.com>
2016-07-25 10:51:06 -0400

    Disable rolling mod interrupt

--- keyboards/ergodox_ez/keymaps/cam/config.h
+++ keyboards/ergodox_ez/keymaps/cam/config.h
@@ -11,4 +11,6 @@
 #define LSPO_KEY KC_9
 #define RSPC_KEY KC_LBRC
 
+#undef IGNORE_MOD_TAP_INTERRUPT // this makes it possible to do rolling combos (zx) with keys that convert to other keys on hold (z becomes ctrl when you hold it, and when this option isn't enabled, z rapidly followed by x actually sends Ctrl-x. That's bad.)
+
 #endif
