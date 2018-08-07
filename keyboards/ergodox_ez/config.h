ead2f2ddf033162b878553a9f195a50199386ac5
Cam Thompson <cam@camthompson.com>
2018-08-07 19:19:04 -0400

    Decrease USB power consumption to work with iPad

--- keyboards/ergodox_ez/config.h
+++ keyboards/ergodox_ez/config.h
@@ -82,6 +82,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
  * manufacturer specs.
  */
 
+#define USB_MAX_POWER_CONSUMPTION 50
 
 // RGB backlight
 #define IS31FL3731_I2C_ADDRESS_1 IS31FL3731_I2C_ADDRESS_GND
