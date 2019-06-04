05daeef20e7b57c1d21f4415d9b41124845dfe63
Cam Thompson <cam@camthompson.com>
2019-06-04 13:33:37 -0400

    s/new_cam/cam

--- keyboards/ergodox_ez/keymaps/cam/config.h
+++ keyboards/ergodox_ez/keymaps/cam/config.h
@@ -1,12 +1,3 @@
-#ifndef CONFIG_USER_H
-#define CONFIG_USER_H
-
 #include "../../config.h"
 
-// Tap left shift for (, right shift for {
-#define LSPO_KEY KC_9
-#define RSPC_KEY KC_LBRC
-
 #undef IGNORE_MOD_TAP_INTERRUPT // this makes it possible to do rolling combos (zx) with keys that convert to other keys on hold (z becomes ctrl when you hold it, and when this option isn't enabled, z rapidly followed by x actually sends Ctrl-x. That's bad.)
-
-#endif
--- keyboards/ergodox_ez/keymaps/cam/keymap.c
+++ keyboards/ergodox_ez/keymaps/cam/keymap.c
@@ -1,4 +1,4 @@
-#include "ergodox.h"
+#include QMK_KEYBOARD_H
 #include "debug.h"
 #include "action_layer.h"
 
@@ -29,21 +29,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 // If it accepts an argument (i.e, is a function), it doesn't need KC_.
 // Otherwise, it needs KC_*
-[BASE] = KEYMAP(  // layer 0 : default
+[BASE] = LAYOUT_ergodox(  // layer 0 : default
         // left hand
-        KC_EQL        , KC_1   , KC_2    , KC_3    , KC_4    , KC_5 , KC_ESC       ,
-        KC_TAB        , KC_Q   , KC_W    , KC_E    , KC_R    , KC_T , KC_BSLS      ,
+        KC_EQL        , KC_1   , KC_2    , KC_3    , KC_4    , KC_5 , KC_ESC          ,
+        KC_TAB        , KC_Q   , KC_W    , KC_E    , KC_R    , KC_T , KC_BSLS         ,
         CTL_T(KC_ESC) , KC_A   , KC_S    , KC_D    , KC_F    , KC_G ,
-        KC_LSPO       , KC_Z   , KC_X    , KC_C    , KC_V    , KC_B , ALL_T(KC_NO) ,
+        KC_LSHIFT     , KC_Z   , KC_X    , KC_C    , KC_V    , KC_B , ALL_T(KC_NO)    ,
         KC_MUTE       , KC_GRV , KC_BSLS , KC_LEFT , KC_RGHT ,
-                                                        KC_LALT      , KC_LCTL     ,
-                                                                       KC_HOME     ,
-                                              KC_ENT , GUI_T(KC_LGUI) , KC_END     ,
+                                                        KC_LALT      , KC_LCTL        ,
+                                                                       KC_HOME        ,
+                                              KC_ENT , GUI_T(KC_LGUI) , KC_END        ,
         // right hand
-        KC_ESC        , KC_6    , KC_7     , KC_8    , KC_9     , KC_0    , KC_MINS ,
-        KC_LBRC       , KC_Y    , KC_U     , KC_I    , KC_O     , KC_P    , KC_RBRC ,
-                        KC_H    , KC_J     , KC_K    , KC_L     , KC_SCLN , KC_QUOT ,
-        MO(MDIA)      , KC_N    , KC_M     , KC_COMM , KC_DOT   , KC_SLSH , KC_RSPC ,
+        KC_ESC        , KC_6    , KC_7     , KC_8    , KC_9     , KC_0    , KC_MINS   ,
+        KC_LBRC       , KC_Y    , KC_U     , KC_I    , KC_O     , KC_P    , KC_RBRC   ,
+                        KC_H    , KC_J     , KC_K    , KC_L     , KC_SCLN , KC_QUOT   ,
+        MO(MDIA)      , KC_N    , KC_M     , KC_COMM , KC_DOT   , KC_SLSH , KC_RSHIFT ,
                         KC_LEFT ,  KC_DOWN , KC_UP   , KC_RIGHT , KC_MPLY ,
         KC_RCTL , KC_RALT,
         KC_PGUP ,
@@ -71,7 +71,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                                 `--------------------'       `--------------------'
  */
 // MDIA
-[MDIA] = KEYMAP(
+[MDIA] = LAYOUT_ergodox(
        // left hand
        KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS ,
        KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS ,
@@ -94,10 +94,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 };
 
 const uint16_t PROGMEM fn_actions[] = {
+
 };
 
 const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
 {
+  // MACRODOWN only works in this function
+      switch(id) {
+        case 0:
+        if (record->event.pressed) {
+          register_code(KC_RSFT);
+        } else {
+          unregister_code(KC_RSFT);
+        }
+        break;
+      }
     return MACRO_NONE;
 };
 
@@ -122,9 +133,6 @@ void matrix_scan_user(void) {
             break;
         case 2:
             ergodox_right_led_2_on();
-            break;
-        case 3:
-            ergodox_right_led_3_on();
             break;
         default:
             // none
