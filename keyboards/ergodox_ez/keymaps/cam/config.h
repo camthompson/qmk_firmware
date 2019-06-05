c847c1d28c225c965038c5d389d0a604604c1c26
Cam Thompson <cam@camthompson.com>
2019-06-05 10:47:11 -0400

    Move power customization to my file

--- keyboards/ergodox_ez/keymaps/cam/config.h
+++ keyboards/ergodox_ez/keymaps/cam/config.h
@@ -1,3 +1,5 @@
 #include "../../config.h"
 
 #undef IGNORE_MOD_TAP_INTERRUPT // this makes it possible to do rolling combos (zx) with keys that convert to other keys on hold (z becomes ctrl when you hold it, and when this option isn't enabled, z rapidly followed by x actually sends Ctrl-x. That's bad.)
+#undef USB_MAX_POWER_CONSUMPTION
+#define USB_MAX_POWER_CONSUMPTION 50 // this lowers power consumption to work with iPad Pro
