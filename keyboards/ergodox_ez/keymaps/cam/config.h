5a1718c5293122db4e72d0eb8efde9864aeaf413
Cam Thompson <cam@camthompson.com>
2019-06-05 10:48:47 -0400

    Reset debounce value

--- keyboards/ergodox_ez/keymaps/cam/config.h
+++ keyboards/ergodox_ez/keymaps/cam/config.h
@@ -3,3 +3,4 @@
 #undef IGNORE_MOD_TAP_INTERRUPT // this makes it possible to do rolling combos (zx) with keys that convert to other keys on hold (z becomes ctrl when you hold it, and when this option isn't enabled, z rapidly followed by x actually sends Ctrl-x. That's bad.)
 #undef USB_MAX_POWER_CONSUMPTION
 #define USB_MAX_POWER_CONSUMPTION 50 // this lowers power consumption to work with iPad Pro
+#define DEBOUNCE 10 // this might help with accidental duplicate key presses
