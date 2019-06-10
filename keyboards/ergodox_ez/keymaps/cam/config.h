8dffb17b3a837ce53552fe00ae86d196d78ab8fb
Cam Thompson <cam@camthompson.com>
2019-06-10 11:42:52 -0400

    Set debounce to 20

--- keyboards/ergodox_ez/keymaps/cam/config.h
+++ keyboards/ergodox_ez/keymaps/cam/config.h
@@ -3,3 +3,4 @@
 #undef IGNORE_MOD_TAP_INTERRUPT // this makes it possible to press caps lock/ctrl key first and then hit another key
 #undef USB_MAX_POWER_CONSUMPTION
 #define USB_MAX_POWER_CONSUMPTION 50 // this lowers power consumption to work with iPad Pro
+#define DEBOUNCE 20 // this might help with accidental duplicate key presses
