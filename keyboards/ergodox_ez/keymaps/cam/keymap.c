f0e9f823d2b0cfe1d76fe00af1836716122d19bb
Cam Thompson <cam@camthompson.com>
2019-12-19 12:26:06 -0500

    Put braces on left

--- keyboards/ergodox_ez/keymaps/cam/keymap.c
+++ keyboards/ergodox_ez/keymaps/cam/keymap.c
@@ -35,9 +35,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB        , KC_Q   , KC_W    , KC_E    , KC_R    , KC_T , KC_BSLS         ,
         CTL_T(KC_ESC) , KC_A   , KC_S    , KC_D    , KC_F    , KC_G ,
         KC_LSHIFT     , KC_Z   , KC_X    , KC_C    , KC_V    , KC_B , ALL_T(KC_NO)    ,
-        KC_MUTE       , KC_GRV , KC_BSLS , KC_LEFT , KC_RGHT ,
-                                                        KC_LALT      , KC_LCTL        ,
-                                                                       KC_HOME        ,
+        KC_MUTE       , KC_GRV , KC_BSLS , KC_LBRC , KC_RBRC ,
+                                                              KC_LALT , KC_LCTL       ,
+                                                                        KC_HOME       ,
                                               KC_ENT , GUI_T(KC_LGUI) , KC_END        ,
         // right hand
         KC_ESC        , KC_6    , KC_7     , KC_8    , KC_9     , KC_0    , KC_MINS   ,
