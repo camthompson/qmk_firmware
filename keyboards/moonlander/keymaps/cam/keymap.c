a4e3a6b207fb327dc1fa7935bff5f053dcd6e2de
Cam Thompson <cam@camthompson.com>
2023-03-24 10:31:13 -0400

    swap layer 1 and 2 keys

--- keyboards/moonlander/keymaps/cam/keymap.c
+++ keyboards/moonlander/keymaps/cam/keymap.c
@@ -14,7 +14,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,         KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,           KC_BSLS,                                        KC_LBRC,        KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,           KC_RBRC,
     LCTL_T(KC_ESCAPE),KC_A,         KC_S,           KC_D,           KC_F,           KC_G,           KC_HYPR,                                        KC_HYPR,        KC_H,           KC_J,           KC_K,           KC_L,           KC_SCLN,        KC_QUOTE,
     KC_LSFT,        KC_Z,           KC_X,           KC_C,           KC_V,           KC_B,                                                           KC_N,           KC_M,           KC_COMMA,       KC_DOT,         KC_SLSH,        KC_RSFT,
-    KC_GRAVE,       RGB_TOG,        OSL(2),         OSL(1),         KC_LALT,        KC_HYPR,                                                        KC_HYPR,        KC_LEFT,        KC_DOWN,        KC_UP,          KC_RIGHT,       MO(1),
+    KC_GRAVE,       RGB_TOG,        OSL(1),         OSL(2),         KC_LALT,        KC_HYPR,                                                        KC_HYPR,        KC_LEFT,        KC_DOWN,        KC_UP,          KC_RIGHT,       MO(1),
     KC_ENTER,       KC_LGUI,        KC_LGUI,                                                                                                        KC_BSPC,        KC_BSPC,        KC_SPC
   ),
   [1] = LAYOUT_moonlander(
