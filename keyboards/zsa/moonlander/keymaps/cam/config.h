e4e317b9b8fb3bf424bcfdf8a1a64746d98cd825
Cam Thompson <cam@camthompson.com>
2025-09-06 11:46:09 -0400

    add pragma, update key codes

--- keyboards/zsa/moonlander/keymaps/cam/config.h
+++ keyboards/zsa/moonlander/keymaps/cam/config.h
@@ -2,6 +2,9 @@
   Set any config.h overrides for your specific keymap here.
   See config.h options at https://docs.qmk.fm/#/config_options?id=the-configh-file
 */
+
+#pragma once
+
 #define ORYX_CONFIGURATOR
-#define RGB_MATRIX_STARTUP_SPD 60
+#define RGB_MATRIX_DEFAULT_SPD 60
 #define HOLD_ON_OTHER_KEY_PRESS
